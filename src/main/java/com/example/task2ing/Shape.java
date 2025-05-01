7896ca1b7dd0988bbcf7aa7d509e019407678318
Nestle20 <nastyaserdyukova3@gmail.com>
2025-05-02 00:29:36 +0300

    Добавление Doc

--- src/main/java/com/example/task2ing/Shape.java
+++ src/main/java/com/example/task2ing/Shape.java
@@ -3,8 +3,13 @@ package com.example.task2ing;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+
 public abstract class Shape {
     protected Color color;
+    /**
+     * Абстрактный класс, представляющий геометрическую фигуру.
+     * Этот класс служит базовым для всех конкретных реализаций фигур.
+     */
     protected double size;
     protected double x, y;
 
@@ -13,10 +18,19 @@ public abstract class Shape {
         this.size = size;
     }
 
+    /**
+     *
+     * @param x
+     * @param y
+     */
     public void setPosition(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     *
+     * @param gc
+     */
     public abstract void draw(GraphicsContext gc);
 }
